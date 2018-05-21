b846402722bae1bd7958633dfcbc6687a54a7a46
0900304132 <369807338@qq.com>
2018-05-21 15:10:29 +0800

    php file

---
+++ class/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/21
+ * Time: 14:03
+ */
+
+defined('DEVELOPMENT') or define('DEVELOPMENT', 1);
+global $API_URL;
+
+$API_URL = '';
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/21
+ * Time: 13:47
+ */
+if(is_file('class/config.php')){
+    include_once 'class/config.php';
+}
+
